ef5937db7914684f2d04748ea9c2e57623fd8656
ManoharMP3 <79815051+ManoharMP3@users.noreply.github.com>
2024-04-13 17:07:34 +0530

    Update music.py

--- music.py
+++ music.py
@@ -1,5 +1,10 @@
 import streamlit as st
-import numpy as np
+from streamlit_webrtc import webrtc_streamer
+import av
+import cv2 
+import numpy as np 
+import mediapipe as mp 
+from keras.models import load_model
 import webbrowser
 
 model = load_model("model.h5")
